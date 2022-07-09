d732a9f4e56e80ab1077d857cbbd391082706ad9
Alex Mamani <alex.mamani@tecsup.edu.pe>
2022-07-09 13:02:47 -0500

    fix: index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,22 @@
+import {Link} from 'react-router-dom'
+import { useAppContext } from '../store/store'
+import Card from '../components/card'
 
-export default function EditPage() {
+export default function IndexPage() {
 
-    return <div>INDEX Page</div>
+    const store = useAppContext();
+
+    return (
+        <>
+            <Link to="/create">Create</Link>
+            <div>INDEX Page</div>
+            {store.dishes.map((item)=>(
+                <Card key={item.id} data={item}></Card>
+
+            ))}
+
+        </>
+    
+    )
+
     }
