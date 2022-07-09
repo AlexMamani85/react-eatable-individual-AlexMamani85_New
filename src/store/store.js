2b1c4f91df5215569b486faa6c0c53fe22b12296
Alex Mamani <alex.mamani@tecsup.edu.pe>
2022-07-09 13:03:18 -0500

    fix: store file

--- src/store/store.js
+++ src/store/store.js
@@ -1,46 +1,58 @@
 import { createContext, useContext, useState, useEffect } from "react";
-import apiFetch from '../services/api-fetch'
+import apiFetch from '../services/api-fetch';
+import {dataReactEatable} from '../config';
 
 const AppContext = createContext({
-    items: [],
-    createItem: ()=>{},
-    getItem: ()=>{},
-    updateItem: ()=>{},
+    dishes: [],
+    createDish: (dish)=>{},
+    getDish: (id)=>{},
+    updateDish: (dish)=>{},
 
 })
 
 
 export default function Store({children}){
-    const [items, setItems] = useState([]);
+    const [dishes, setDishes] = useState([]);
 
     useEffect(()=>{
-        apiFetch("products").then(setItems);       
+
+        
+        apiFetch("products").then(x=>{
+            localStorage.setItem(dataReactEatable, JSON.stringify(x)) //discoDuro
+            setDishes(x)    //memoriaRam
+        });
+   
     },[])
-    
-    function createItem(item) {
-        const temp = [...items];
-        temp.push(item);
-        setItems(temp);
+
+    function createDish(newDish) {
+        const temp = [...JSON.parse(localStorage.getItem(dataReactEatable))];
+        temp.push(newDish);
+        setDishes(temp);
+        localStorage.setItem(dataReactEatable, JSON.stringify(temp));
     }
 
-    function getItem(id) {
-        const item = items.find((item)=>item.id===id)
+    function getDish(id) {
+        const actualDishes = dishes || localStorage.getItem(dataReactEatable);
+        const item = actualDishes.find((item)=>Number(item.id)===Number(id))
+        console.log(item);
         return item;
     }
 
-    function updateItem(item) {
-        const index = items.findIndex((i)=>i.id===item.id);
-        const temp = [...items];
+    function updateDish(item) {
+        const actualDishes = dishes || localStorage.getItem(dataReactEatable);
+        const index = actualDishes.findIndex((i)=>i.id===item.id);
+        const temp = [...actualDishes];
         temp[index] = {...item}
+        localStorage.setItem(dataReactEatable, JSON.stringify(temp));
     }
 
     return (
         <AppContext.Provider
             value={{
-                items,
-                createItem,
-                getItem,
-                updateItem,
+                dishes,
+                createDish,
+                getDish,
+                updateDish,
             }}
         >
         {children}
