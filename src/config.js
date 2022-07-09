f719c0ab514a36025c58bb33a216341b1a0887dc
Alex Mamani <alex.mamani@tecsup.edu.pe>
2022-07-09 13:02:06 -0500

    fix: config file

--- src/config.js
+++ src/config.js
@@ -1,2 +1,5 @@
 export const tokenKey = "react_eatable";
+export const dataReactEatable = "data_react_eatable";
+export const actualIdEatable = "actual_id_eatable";
+
 export const BASE_URI = "https://react-eatable-api.herokuapp.com/";
